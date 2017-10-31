72960fa0f857f7763a2c6fa8ca410fd4de70c3de
BuruhinSergei <33168204+GreyMag2141@users.noreply.github.com>
2017-10-31 16:34:08 +1000

    Update Ex110.java

--- src/Zadacha/Ex1/Ex110.java
+++ src/Zadacha/Ex1/Ex110.java
@@ -1,12 +1,16 @@
-package Zadacha.Ex1;
-
 import java.util.Scanner;
 
 public class Ex110 {
-            public static void main(String[] args) {
-                Scanner in= new Scanner(System.in);
-            String name= in.nextLine();
-        int age = in.nextInt();
-    String city = in.nextLine();//напишите тут ваш код
-}
+    public static void main(String[] args) {
+        System.out.println("Программа находит процент P от суммы S. ");
+        Scanner in= new Scanner(System.in);
+        System.out.print("Введите процент: ");
+        double p =in.nextDouble();
+        System.out.print("Введите сумму ");
+        double s =in.nextDouble();
+        double x = (s/100)*p;
+        System.out.println("= " + x);
+
+
     }
+}
